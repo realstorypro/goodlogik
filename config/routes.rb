fae33316d2e7ee7afc29c28f05851ab1b6f87887
Leonid Medovyy <leonid@logik.works>
2018-11-21 10:54:36 -0700

    setting up basic navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'demo/request'
   root 'pages#home'
-  get 'pages/home'
+  get 'agencies', to: 'pages#agencies'
 
   post 'demo/send_request', to: 'demo#send_request'
 end
