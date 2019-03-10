021d0ff72ba89c2086056cb2186a487409f36950
Leonid Medovyy <leonid@logik.works>
2019-03-10 06:33:08 +0530

    setting up 3d

---
+++ frontend/js/cover.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    Scene
+        Property name="clearColor" color="#FFF"
+        Camera type="arcRotate"
+        Entity(:position="[0, 0, 5]")
+            Animation(property="rotation.x" :duration="25" :end="Math.PI * 2")
+            Animation(property="rotation.y" :duration="25" :end="Math.PI * 2")
+            Animation(property="rotation.z" :duration="25" :end="Math.PI * 2")
+            PointLight(diffuse="#FFF")
+            template(v-for="x in squares")
+                template(v-for="y in squares")
+                    Box(v-for="z in squares" :position="[x, y, z]" :key="`${x},${y},${z}`")
+                        Animation(property="rotation.x" :duration="5" :end="Math.PI * 2")
+                        Animation(property="rotation.y" :duration="5" :end="Math.PI * 2")
+                        Animation(property="rotation.z" :duration="5" :end="Math.PI * 2")
+</template>
+
+<script lang="coffee">
+export default {
+    data: ->
+        squares: [0, 2, -2 ]
+}
+</script>
+
+<style lang="sass">
+    canvas
+        height: 100vh
+</style>
