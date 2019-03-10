b31b1fdd989ea2b6bdee5080978e505133e3a527
Leonid Medovyy <leonid@logik.works>
2019-03-10 12:04:39 +0530

    updating the ladning apge

--- frontend/js/cover.vue
+++ frontend/js/cover.vue
@@ -1,24 +1,63 @@
 <template lang="pug">
     Scene
-        Property name="clearColor" color="#FFF"
-        Camera type="arcRotate"
+        Property(name="clearColor" :color="background")
+        Entity
+            Camera(type="arcRotate" :radius="1")
+        HemisphericLight(:diffuse="hemispheric_light")
         Entity(:position="[0, 0, 5]")
-            Animation(property="rotation.x" :duration="25" :end="Math.PI * 2")
-            Animation(property="rotation.y" :duration="25" :end="Math.PI * 2")
-            Animation(property="rotation.z" :duration="25" :end="Math.PI * 2")
-            PointLight(diffuse="#FFF")
+            Animation(property="rotation.x" :duration="15" :end="Math.PI * 2")
+            Animation(property="rotation.y" :duration="15" :end="Math.PI * 2")
+            Animation(property="rotation.z" :duration="15" :end="Math.PI * 2")
+            PointLight(:diffuse="point_light")
             template(v-for="x in squares")
                 template(v-for="y in squares")
                     Box(v-for="z in squares" :position="[x, y, z]" :key="`${x},${y},${z}`")
-                        Animation(property="rotation.x" :duration="5" :end="Math.PI * 2")
-                        Animation(property="rotation.y" :duration="5" :end="Math.PI * 2")
-                        Animation(property="rotation.z" :duration="5" :end="Math.PI * 2")
+                        Material(:diffuse="pick_color(x,y,z)" :metalic="50" :roughness="1")
+                        Animation(property="rotation.x" :duration="200" :end="Math.PI * 2")
+                        Animation(property="rotation.y" :duration="150" :end="Math.PI * 2")
+                        Animation(property="rotation.z" :duration="50" :end="Math.PI * 2")
 </template>
 
 <script lang="coffee">
 export default {
     data: ->
+        background: '#fff'
+        hemispheric_light: '#eeeeee'
+        point_light: '#000000'
+        square_color: "#000"
         squares: [0, 2, -2 ]
+        colors: ['blue', 'orange', 'yellow', 'black',
+            'yellow', 'purple', 'blue', 'orange',
+            'orange', 'blue', 'purple', 'yellow',
+            'orange', 'purple', 'blue', 'yellow'
+        ]
+    mounted: ->
+        console.log "mounting complete"
+    methods:
+        pick_color: (x, y, z)->
+
+            if x is 0 and y is 2
+                '#666'
+            else if x is 2 and y is 2
+                '#ccc'
+            else if x is -2 and y is 2
+                '#ddd'
+
+            else if x is 0 and y is 0
+                '#0aa650'
+            else if x is 2 and y is 0
+                '#fce608'
+            else if x is -2 and y is 0
+                '#0aa650'
+
+
+            else if x is 0 and y is -2
+                '#00b4d5'
+            else if x is 2 and y is -2
+                '#655da6'
+            else if x is -2 and y is -2
+                '#f8a61d'
+
 }
 </script>
 
