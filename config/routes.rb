73cf326ea3a0dbf6ad6d113fe3ad4840673b477c
Leonid Medovyy <leonid@logik.works>
2019-11-30 20:39:36 -0800

    setting up tos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#landing'
-  get 'lander', to: 'pages#landing'
+  get 'privacy', to: 'pages#privacy'
+  get 'tos', to: 'pages#tos'
 
   post 'sales/send_request', to: 'sales#send_request'
   post 'discovery/send_request', to: 'discovery#send_request'
