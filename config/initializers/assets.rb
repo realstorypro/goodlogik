6e8862371e067189bf327bccfcaa83959ea7f41b
Leonid Medovyy <leonid@logik.works>
2019-12-12 19:10:46 -0800

    updated precompiled assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(logo.png background.mp4)
+Rails.application.config.assets.precompile += %w(logo.png logo-sm.png about.mp3)
