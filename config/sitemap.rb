5ea6c3eb66acf45907384c94e23a01799688bacf
Leonid Medovyy <leonid@logik.works>
2019-12-15 07:46:54 -0800

    added sitemap removed feedjira

---
+++ config/sitemap.rb
@@ -0,0 +1,31 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.goodlogik.com"
+
+SitemapGenerator::Sitemap.create do
+  add root_path, :priority => 0.7, :changefreq => 'daily'
+  add features_path, :priority => 0.7, :changefreq => 'daily'
+  add pricing_path, :priority => 0.7, :changefreq => 'daily'
+  add about_path, :priority => 0.7, :changefreq => 'daily'
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+end
