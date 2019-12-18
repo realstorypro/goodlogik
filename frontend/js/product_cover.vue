077f23db1c8214c5555e24a1c139beec81453821
Leonid Medovyy <leonid@logik.works>
2019-12-18 15:30:11 -0800

    setup product page

--- frontend/js/product_cover.vue
+++ frontend/js/product_cover.vue
@@ -7,7 +7,7 @@ import * as BABYLON from 'babylonjs'
 export default {
     mounted: ->
         $('.overlay').css('transition', 'all 2.5s')
-        $('.overlay').css('opacity', 0.8)
+        $('.overlay').css('opacity', 0.39)
 
         canvas = @.$refs["canvas"]
 
@@ -25,7 +25,9 @@ export default {
             scene.clearColor = new BABYLON.Color3.Blue()
 
             # Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-            camera = new (BABYLON.FreeCamera)('camera1', new (BABYLON.Vector3)(0, 5, -10), scene)
+            camera = new BABYLON.ArcRotateCamera("Camera", 1.0, 2.0, -3.3, new BABYLON.Vector3(0, 0, 0), scene);
+            camera.useBouncingBehavior = true
+            camera.useAutoRotationBehavior = true
 
             # Target the camera to scene origin
             camera.setTarget BABYLON.Vector3.Zero()
@@ -40,9 +42,18 @@ export default {
             # Scene Settings
             box_positions = [0, 1.5, -1.5]
             frameRate = 25
+            animation_time = 45
 
             # Boxes Array
             boxes = []
+
+            # Colors Array
+            colors = []
+            colors.push new BABYLON.Color3.FromHexString('#00bcf2')
+            colors.push new BABYLON.Color3.FromHexString('#f5c647')
+            colors.push new BABYLON.Color3.FromHexString('#9578b6')
+            colors.push new BABYLON.Color3.FromHexString('#f06160')
+            colors.push new BABYLON.Color3.FromHexString('#f06160')
 
             for x in box_positions
                 for y in box_positions
@@ -51,14 +62,26 @@ export default {
                         box = BABYLON.MeshBuilder.CreateBox("box_#{x}#{y}#{z}", {}, scene)
                         box.position = new BABYLON.Vector3(x, y, z)
 
-                        x_animation = buildAnimation(x, "x","#{x}#{y}#{z}", frameRate)
-                        scene.beginDirectAnimation(box, [x_animation], 0, 2 * frameRate, true)
+                        x_animation = buildAnimation(x, "x","#{x}#{y}#{z}", frameRate, animation_time)
+                        scene.beginDirectAnimation(box, [x_animation], 0, animation_time * frameRate, false)
 
-                        y_animation = buildAnimation(y, "y","#{x}#{y}#{z}", frameRate)
-                        scene.beginDirectAnimation(box, [y_animation], 0, 2 * frameRate, true)
+                        y_animation = buildAnimation(y, "y","#{x}#{y}#{z}", frameRate, animation_time)
+                        scene.beginDirectAnimation(box, [y_animation], 0, animation_time * frameRate, false)
 
-                        z_animation = buildAnimation(z, "z","#{x}#{y}#{z}", frameRate)
-                        scene.beginDirectAnimation(box, [z_animation], 0, 2 * frameRate, true)
+                        z_animation = buildAnimation(z, "z","#{x}#{y}#{z}", frameRate, animation_time)
+                        scene.beginDirectAnimation(box, [z_animation], 0, animation_time * frameRate, false)
+
+                        material = new BABYLON.StandardMaterial("material_#{x}#{y}#{z}", scene)
+                        material.alpha = 0.85
+                        material.wireframe = true
+
+                        # shuffling and picking a random color
+                        shuffled_colors = shuffle(colors)
+                        [...,picked_color] = shuffled_colors
+
+                        material.diffuseColor = picked_color
+
+                        box.material = material
 
                         boxes << box
 
@@ -66,7 +89,17 @@ export default {
             # Return the created scene
             scene
 
-        buildAnimation = (position, axis, identifier,frameRate) ->
+        # shuffles an array
+        shuffle = (a) ->
+            i = a.length
+            while --i > 0
+                j = ~~(Math.random() * (i + 1)) # ~~ is a common optimization for Math.floor
+                t = a[j]
+                a[j] = a[i]
+                a[i] = t
+            a
+
+        buildAnimation = (position, axis, identifier,frameRate, animation_time) ->
             animation = new BABYLON.Animation("box_position_#{axis}_#{identifier}_#{position}", "position.#{axis}", frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE)
 
             range = Math.random() * (1.2 - 1.1) + 1.1
@@ -76,10 +109,10 @@ export default {
                 frame: 0
                 value: position
             keyFrames.push
-                frame: frameRate
+                frame: frameRate * animation_time / 2
                 value: position * range
             keyFrames.push
-                frame: 2 * frameRate
+                frame: animation_time * frameRate
                 value: position
 
             animation.setKeys(keyFrames)
